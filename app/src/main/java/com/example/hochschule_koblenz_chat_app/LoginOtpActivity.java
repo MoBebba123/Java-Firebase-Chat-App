10ab39f818e5ddb3c224f3c125d70a8da40afcba
mohamedbebba <mohamedbebba1@gmail.com>
2024-07-11 00:03:19 +0200

    connect to firebase

--- app/src/main/java/com/example/hochschule_koblenz_chat_app/LoginOtpActivity.java
+++ app/src/main/java/com/example/hochschule_koblenz_chat_app/LoginOtpActivity.java
@@ -1,6 +1,11 @@
 package com.example.hochschule_koblenz_chat_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginOtpActivity extends AppCompatActivity {
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginOtpActivity extends AppCompatActivity {
+    String phoneNumber;
+    Long timeoutSeconds = 60L;
+    String verificationCode;
+
+    EditText otpInput;
+    Button nextBtn;
+    ProgressBar progressBar;
+    TextView resendOtpTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_otp);
+
+         phoneNumber = getIntent().getExtras().getString("phone");
+
+        Toast.makeText(getApplicationContext(), phoneNumber, Toast.LENGTH_LONG).show();
 
     }
 }
