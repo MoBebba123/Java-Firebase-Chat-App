ff53119de8c2011520bb899a3534c7735033d34c
mohamedbebba <mohamedbebba1@gmail.com>
2024-07-11 11:32:50 +0200

    create models

--- app/src/main/java/com/example/hochschule_koblenz_chat_app/LoginUsernameActivity.java
+++ app/src/main/java/com/example/hochschule_koblenz_chat_app/LoginUsernameActivity.java
@@ -1,20 +1,66 @@
 package com.example.hochschule_koblenz_chat_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginUsernameActivity extends AppCompatActivity {
+import com.example.hochschule_koblenz_chat_app.model.UserModel;
+import com.example.hochschule_koblenz_chat_app.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 
+public class LoginUsernameActivity extends AppCompatActivity {
+    EditText usernameInput;
+    Button letMeInBtn;
+    ProgressBar progressBar;
+    String phoneNumber;
+    UserModel userModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_username);
 
+        usernameInput = findViewById(R.id.login_username);
+        letMeInBtn = findViewById(R.id.login_let_me_in_btn);
+        progressBar =findViewById(R.id.login_progress_bar);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        getUsername();
+
+    }
+    void getUsername(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    userModel =    task.getResult().toObject(UserModel.class);
+                    if(userModel!=null){
+                        usernameInput.setText(userModel.getUsername());
+                    }
+                }
+            }
+        });
+    }
+
+    void setInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            letMeInBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            letMeInBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
